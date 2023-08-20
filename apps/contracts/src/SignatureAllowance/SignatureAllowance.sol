3ed4e65f8c07408a8dbcf8190b1410931ec3f764
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-08-20 17:03:21 +0530

    fix: added condition to ensure token is allowlisted before withdrawal

--- apps/contracts/src/SignatureAllowance/SignatureAllowance.sol
+++ apps/contracts/src/SignatureAllowance/SignatureAllowance.sol
@@ -29,7 +29,7 @@ contract SignatureAllowance is
     /// but the token is already allowlisted
     error TokenAlreadyInAllowlist();
 
-    /// When a signer tries to remove a token from allowlist
+    /// When a signer tries to remove or withdraw a token from allowlist
     /// but the token is not allowlisted
     error TokenNotInAllowlist();
 
@@ -129,6 +129,10 @@ contract SignatureAllowance is
         // prevent replay attack
         if (saltUsed[_salt]) {
             revert SaltAlreadyUsed(_salt);
+        }
+        // check if token is allowlisted
+        if(!tokensAllowed[_token]){
+            revert TokenNotInAllowlist();
         }
         // check if the signature(s) is valid and active
         isSignatureValid(
